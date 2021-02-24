621cb95cef4412927f5d4356bc47c7519106c121
mateusdeitos <mate.deitos@hotmail.com>
2021-02-23 21:51:53 -0300

    dia 2 - next.js

---
+++ frontend/moveit-next/src/components/Countdown.tsx
@@ -0,0 +1,79 @@
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import styles from '../styles/components/Countdown.module.css';
+
+export const Countdown = () => {
+    const [initialTime] = useState(25);
+    const [time, setTime] = useState(initialTime * 60);
+    const [active, setActive] = useState(false);
+    const [buttonText, setButtonText] = useState('Iniciar um ciclo');
+    const [myTimeOut, setMyTimeOut] = useState<NodeJS.Timeout>(null);
+
+    useEffect(() => {
+        if (!active) {
+            clearTimeout(myTimeOut);
+        }
+        if (active && time > 0) {
+            setMyTimeOut(
+                setTimeout(() => {
+                    setTime(time - 1);
+                }, 1000)
+            );
+        }
+
+        if (time === 0) {
+            setButtonText('Reiniciar Timer');
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [active, time])
+
+    const splittedMinutes = useMemo(() => {
+        return String(Math.floor(time / 60)).padStart(2, '0').split('');
+    }, [time])
+
+    const splittedSeconds = useMemo(() => {
+        return String(time % 60).padStart(2, '0').split('');
+    }, [time])
+
+    const [minuteLeft, minuteRight] = splittedMinutes;
+    const [secondLeft, secondRight] = splittedSeconds;
+
+    const toggleCountDown = useCallback(() => {
+        if (time > 0) {
+            if (!active) {
+                setButtonText('Pausar ciclo');
+            } else if (time < initialTime * 60) {
+                setButtonText('Resumir ciclo');
+            } else {
+                setButtonText('Iniciar um ciclo');
+            }
+            setActive(prev => !prev);
+        } else {
+            setTime(initialTime * 60);
+            setActive(false);
+            setButtonText('Iniciar um ciclo');
+        }
+    }, [active, initialTime, time])
+
+    return (
+        <div>
+
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
+            </div>
+            <button
+                type="button"
+                onClick={toggleCountDown}
+                className={styles.countdownButton}>
+                {buttonText}
+            </button>
+        </div>
+    )
+}
---
+++ frontend/moveit-next/src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css';
+
+export const Profile = () => {
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/mateusdeitos.png" alt="Mateus Deitos" />
+            <div>
+                <strong>Mateus Deitos</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level 1
+                </p>
+            </div>
+        </div>
+    )
+}
